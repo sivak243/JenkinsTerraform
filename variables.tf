13987a13e103e4d815e067b22633ae3437a59914
sivak243 <108894267+sivak243@users.noreply.github.com>
2024-05-04 23:29:09 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "ap-east-2" 
+       default     = "us-east-2" 
 }
 
 variable "key_name" { 
