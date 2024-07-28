9bef0632d69559ffc589c65d8bf699e717e51e37
sivak243 <108894267+sivak243@users.noreply.github.com>
2024-07-28 23:03:40 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "jenkins-terraform244"
+    bucket = "jenkins-terraform243"
     key = "main" 
     region = "ap-south-1"
     dynamodb_table = "terraform-state-table"
