35bc7d7a72b485fc2ddbf85850e6669944c28a20
sivak243 <108894267+sivak243@users.noreply.github.com>
2024-07-29 00:25:56 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "ohiokey"   # This is the key used to login Jenkins Machine.
+    default     =  "MyohioKey"   # This is the key used to login Jenkins Machine.
 }
 
 variable "instance_type" { 
